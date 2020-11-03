e1af1bc45ce40fac37c7b0427a8b8c144250b96f
amolsuraj <mr.a.m.jagtap@gmail.com>
2020-11-03 15:27:55 +0530

    Animation

---
+++ app/src/main/java/siddhi/tech/animationdemo/GitCommitTask.java
@@ -0,0 +1,10 @@
+package siddhi.tech.animationdemo;
+
+import android.widget.Toast;
+
+public class GitCommitTask extends MainActivity{
+    public void show()
+    {
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    }
+}
